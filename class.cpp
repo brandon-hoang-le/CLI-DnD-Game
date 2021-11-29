a12506d164977a9406fba7284bc5bfd5972f4c5b
brandon-hoang-le <55907638+brandon-hoang-le@users.noreply.github.com>
2021-11-28 16:20:25 -0800

    Add files via upload

---
+++ class.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <string>
+#include <memory>
+#include <time.h>
+
+using namespace std;
+
+void lineBreak(){
+	cout << "=========================================================" << endl;
+}
+
+class Character {
+protected:
+	string name;
+	int health; 
+	int armor; 
+	int maxHealth; 
+	int attkDmg; 
+	int numberOfAttkDie; 
+	int attkDieSides; 
+	int firstTurnRoll; 
+	int damageTaken; 
+public:
+	Character() {}
+	int getAttkDamage() {
+		return attkDmg; 
+	}
+	int getHP() {
+		return health; 
+	}
+	int getFirstTurn() {
+		return firstTurnRoll; 
+	}
+	string getName() {
+		return name; 
+	}
+	virtual void firstTurn() {
+		firstTurnRoll = rand() % 20 + 1; 
+		cout << name << " rolls one die (1-20): Initiative score of " << firstTurnRoll << endl;
+
+	}
+	virtual void rollAttkDie() {
+		int attkDmg = rand() % attkDieSides + 1;
+		cout << name << " rolls one dice (1-" << attkDieSides << "): Damage score of " << attkDmg << "." << endl;
+	}
+	virtual void receiveDamage(int dmg) {
+		damageTaken = dmg - armor; 
+
+		health = health - damageTaken; 
+		if (health < 0) {
+			health = 0;
+		}
+		cout << name << "'s total HP now at: " << health << endl; 
+	
+	}
+
+	virtual void introduction() = 0;
+	virtual void endFight() = 0; 
+	virtual void printStats() = 0; 
+};
+
+class Warrior : public Character {
+private:
+	int bonusAttk;
+
+public:
+	Warrior(string n) : Character() {
+		name = n; 
+		health = 20;
+		armor = 5;
+		maxHealth = 20;
+		bonusAttk = 5;
+		numberOfAttkDie = 1;
+		attkDieSides = 6;
+	}
+	virtual void rollAttkDie() {
+		int rolls[2];
+		rolls[0] = rand() % attkDieSides + 1;
+		rolls[1] = rand() % attkDieSides + 1;
+		attkDmg = rolls[0] + rolls[1];
+		cout << name << " rolls two dice (1-" << attkDieSides << "): Damage score of " << rolls[0] << " + " << rolls[1] << " + Strength modifier of " << bonusAttk << " for a total of " << attkDmg + bonusAttk << "." << endl;
+		attkDmg += bonusAttk;
+		
+	}
+	virtual void introduction() {
+		cout << "You are " << name << ", a ferocious Warrior." << endl;
+	}
+	virtual void endFight() {
+		if (health == 0) {
+			cout << "You have died..." << endl; 
+		}
+	}
+
+	virtual void printStats() {
+		cout << "Warrior " << name << "'s current stats: " << endl
+			<< "Health: " << health << endl
+			<< "Base Attack Die: " << numberOfAttkDie << " D " << attkDieSides << " S" << endl
+			<< "Armor: " << armor << endl;
+	}
+};
+
+class Monster : public Character{
+public:
+    Monster() : Character(){
+		name = "Monster";
+        health = 10;
+		armor = 0;
+		maxHealth = 10;
+		attkDieSides = 4;
+		numberOfAttkDie = 1; 
+    }
+	Monster(string n) : Character(){
+		name = n;
+        health = 10;
+		attkDieSides = 4;
+		armor = 3;
+		numberOfAttkDie = 2; 
+    }
+    virtual void introduction() {
+        cout << "An enemy " << name << " appears and is ready to battle!" << endl;
+	}
+	virtual void endFight() {
+		if (health == 0)
+		{
+			cout << name << " has been slain!" << endl; 
+            cout << "You won the fight!" << endl << endl;
+		}
+	}
+	virtual void rollAttkDie() {
+		int rolls[2];
+		rolls[0] = rand() % attkDieSides + 1;
+		rolls[1] = rand() % attkDieSides + 1;
+		attkDmg = rolls[0] + rolls[1];
+		cout <<  name << " rolls two dice (1-20): Damage score of " << rolls[0] << " + " << rolls[1] << " for a total of " << attkDmg << "." << endl;
+		
+	}
+	virtual void printStats() {
+		cout << name << "'s current stats: " << endl
+			<< "Health: " << health << endl
+			<< "Base Attack Die: " << numberOfAttkDie << " D " << attkDieSides << " S " << endl
+			<< "Armor: " << armor << endl;
+	}
+};
---
+++ main.cpp
@@ -0,0 +1,127 @@
+#include "class.cpp"
+
+string playerChooseName(){
+	string name;
+	cout << "What is your character name?" << endl; 
+	cin >> name;
+
+	cout << endl;
+	system("CLS");
+	
+	return name;
+}
+void introduceEnemyAndBattleStats(const shared_ptr<Character>& enemy, const shared_ptr<Character>& player){
+		enemy->introduction();
+
+		lineBreak();
+		player->printStats();
+		cout << endl;
+		enemy->printStats();
+		lineBreak();
+
+}
+
+void decideFirstTurn(const shared_ptr<Character>& enemy, const shared_ptr<Character>& player, bool& firstPlayer, bool& secondPlayer){
+	player->firstTurn();
+	enemy->firstTurn();
+
+	// decides first turn
+	while (firstPlayer == false && secondPlayer == false) {
+		if (player->getFirstTurn() > enemy->getFirstTurn()) {
+			firstPlayer = true;
+		}
+		else if (player->getFirstTurn() < enemy->getFirstTurn()) {
+			secondPlayer = true;
+		}
+		else {
+			cout << endl;
+			cout << "Both characters rolled the same number. Rerolling..." << endl; 
+			cout << endl;
+			player->firstTurn();
+			enemy->firstTurn();
+		}
+	}
+}
+
+void battleTurn(shared_ptr<Character> &enemy, shared_ptr<Character> &player, bool &firstPlayer, bool &secondPlayer){
+	while (enemy->getHP() > 0 && player->getHP() > 0) {
+		int choice;
+		if (firstPlayer) {
+			
+			cout << endl << "Your Move:" << endl;
+			cout << "What will you do?" << endl;
+			cout << "1: Attack" << endl;
+			cin >> choice;
+			while (choice != 1) {
+				try {
+					throw choice;
+				}
+				catch (...) {
+					cout << "Invalid output." << endl;
+					cin >> choice;
+				}
+			}
+
+			cout << endl;
+
+			// player chooses attack
+			if (choice == 1) {
+				player->rollAttkDie(); 
+				enemy->receiveDamage(player->getAttkDamage()); 
+
+				if (enemy->getHP() > 0) {
+					firstPlayer = false;
+					secondPlayer = true;
+				}
+			}
+		}
+		// enemy attacks
+		else if (secondPlayer) {
+			enemy->rollAttkDie(); 
+			player->receiveDamage(enemy->getAttkDamage()); 
+
+			if (player->getHP() > 0) {
+				firstPlayer = true;
+				secondPlayer = false;
+			}
+		}
+	}
+}
+void endBattle(shared_ptr<Character> &enemy, shared_ptr<Character> &player){
+	if (player->getHP() == 0) {
+		player->endFight();
+		exit(1);
+	}
+	else {
+		enemy->endFight();
+	}
+
+	system("pause");
+	system("CLS");
+	
+	enemy = make_shared<Monster>(); // new enemy
+}
+void battleSequence() {
+	shared_ptr<Character> player = make_shared<Warrior>(playerChooseName()); // initialize player
+	shared_ptr<Character> enemy = make_shared<Monster>(); // initialize enemy 
+
+	player->introduction();
+
+	while (true) {
+		bool firstPlayer = false, secondPlayer = false;
+
+		introduceEnemyAndBattleStats(enemy, player);
+		decideFirstTurn(enemy, player, firstPlayer, secondPlayer);
+		battleTurn(enemy, player, firstPlayer, secondPlayer);
+		endBattle(enemy, player);
+	}
+}
+
+int main() {
+	srand((unsigned int)time(0));
+	
+	battleSequence(); 
+	
+	system("pause");
+	return 0;
+}
